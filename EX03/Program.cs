ed591bfdc6945393c783bde4375e28178cc9a8f2
von_Krolikoff <milkbrother@yandex.ru>
2022-07-13 22:41:38 +0300

    +EX03 не доделано

---
+++ EX03/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("Задайте размеры массива mXv");
+int m = Input("Введите m: ");
+int n = Input("Введите n: ");
+int[,] array = new int[m, n];
+int x = 0;
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PourArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PourArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+for (x=0, x < m, x++)
+{
+    
+}
