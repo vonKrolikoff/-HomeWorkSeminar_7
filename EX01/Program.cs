e51d87cac624a66a0b1b687141a3a832c0775a4b
von_Krolikoff <milkbrother@yandex.ru>
2022-07-13 21:11:27 +0300

    +EX01

---
+++ EX01/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int m = Input("Введите m: ");
+int n = Input("Введите n: ");
+
+int[,] array = new int[m, n];
+
+void PourArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+PourArray(array);
+PrintArray(array);
+Console.WriteLine();
